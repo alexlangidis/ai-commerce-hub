824c86940f63aa4390ba8f917b6527b37b7f4cfb
Alex Langidis <alexlag.dev@gmail.com>
2026-07-03 13:07:18 +0300

    set up auth database

---
+++ lib/auth.ts
@@ -0,0 +1,17 @@
+import { betterAuth } from "better-auth";
+import { nextCookies } from "better-auth/next-js";
+import { drizzleAdapter } from "@better-auth/drizzle-adapter";
+
+import * as schema from "@/db/schema";
+import { db } from "@/lib/db";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+    schema,
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  plugins: [nextCookies()],
+});
